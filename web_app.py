b73678a103e08844ae74f003ffeb523054ab365e
BransinO1 <corbinmeacham65@gmail.com>
2026-04-22 00:03:25 -0600

    testing app create for database

--- web_app.py
+++ web_app.py
@@ -231,10 +231,13 @@ def delete_incident(incident_id):
 
     return redirect(url_for("home"))
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
+# if __name__ == "__main__":
+#     with app.app_context():
+#         db.create_all()
 
-    from waitress import serve
-    port = int(os.environ.get("PORT", 5000))
-    serve(app, host="0.0.0.0", port=port)
+#     from waitress import serve
+#     port = int(os.environ.get("PORT", 5000))
+#     serve(app, host="0.0.0.0", port=port)
+
+with app.app_context():
+    db.create_all()
