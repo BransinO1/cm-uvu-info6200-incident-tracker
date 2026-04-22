cb013d48de15271c8ea892eabbea4d0cc9a3c3a2
BransinO1 <corbinmeacham65@gmail.com>
2026-04-21 23:56:44 -0600

    updated for postgress

--- web_app.py
+++ web_app.py
@@ -51,8 +51,6 @@ def init_db():
     with app.app_context():
         db.create_all()
 
-init_db()
-
 # HOME ROUTE
 @app.route("/")
 def home():
@@ -234,6 +232,9 @@ def delete_incident(incident_id):
     return redirect(url_for("home"))
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
     from waitress import serve
     port = int(os.environ.get("PORT", 5000))
     serve(app, host="0.0.0.0", port=port)
